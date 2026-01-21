ef0f69bf00bfa0de222ea85e6bddf66766ecf2a7
josedm515 <jdiazm0314@gmail.com>
2026-01-21 07:49:36 -0500

    Fix: remove unused variable

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ export default function CobrApp() {
     }
     
     try {
-      const { data, error } = await supabase
+     const { error } = await supabase
         .from('facturas')
         .insert([
           {
